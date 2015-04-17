fdbf5429f8d099d1ab76de44e953cc9f64b2d3f1
Tzujen Lin <brianlin224@gmail.com>
2015-04-18 00:37:42 +0800

    start content RWD
    完成一個pc版, 思考RWD時如何配置

--- javascripts/tel-bank.js
+++ javascripts/tel-bank.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
-	var $quest    = $('.quest'),
-		$selecter = $('.selecter');
+	var $quest         = $('.quest'),
+		$selecter      = $('.selecter'),
+		$tabList       = $('.tab-list'),
+		$switchContent = $('.switch-content');
 
 	$quest.on('click', function(){
 		$(this).parent().toggleClass('open').siblings().removeClass('open');
+	});
+
+	$tabList.on('click', function(){
+		var _idx = $(this).index();
+
+		$(this).addClass('curr').siblings().removeClass('curr');
+		$switchContent.removeClass('curr').eq(_idx).addClass('curr');
 	});
 
 	$selecter.hover(function(){
