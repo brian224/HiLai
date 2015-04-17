d3d4cd11d21a41b3547d71e3af7cb56cadc018be
Tzujen Lin <brianlin224@gmail.com>
2015-04-17 16:15:50 +0800

    build tel-bank
    backup

--- javascripts/tel-bank.js
+++ javascripts/tel-bank.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
-  var $quest = $('.quest');
+	var $quest    = $('.quest'),
+		$selecter = $('.selecter');
 
-  $quest.on('click', function(){
-    $(this).parent().toggleClass('open').siblings().removeClass('open');
-  });
+	$quest.on('click', function(){
+		$(this).parent().toggleClass('open').siblings().removeClass('open');
+	});
+
+	$selecter.hover(function(){
+		$(this).addClass('open').removeClass('closed');
+	}, function(){
+		$(this).addClass('closed').removeClass('open');
+	});
 });
